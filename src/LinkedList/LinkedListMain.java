39ee9658ad9c1ab582255918f331a58795b9c369
MohanRaj <mraj86698@gmail.com>
2022-07-15 19:49:27 +0530

    Display Message

---
+++ src/LinkedList/LinkedListMain.java
@@ -0,0 +1,11 @@
+package LinkedList;
+
+public class LinkedListMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is an Linked List");
+
+	}
+
+}
